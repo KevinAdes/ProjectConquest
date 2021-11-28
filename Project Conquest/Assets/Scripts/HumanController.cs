1caa9612edb1610d825f2c6b4116d7373e3edcfb
KevinAdes <kra18@hampshire.edu>
2021-11-28 01:19:05 -0500

    Attack Vol 4
    BIg big combat progress. Added a dummy human with almost everything it needs. It takes damage, knockback, dies, and once its dead it becomes a corpse with drinkable blood.
    
    Overhauled hitboxes, made QoL improvemets. a lot needs to be done. a lot has been done. bad post. need to do this more often so i dont forget what im doing. wheeee

---
+++ Project Conquest/Assets/Scripts/HumanController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions.Must;
+
+public class HumanController : MonoBehaviour
+{
+    [Header("Stats")]
+    public float health;
+    public float damage;
+    public float defense;
+    public int expYield;
+
+    [Header ("Components")]
+    public Animator animator;
+    public Collider2D collider2;
+
+    public bool vulerable = true;
+
+
+    PlayerMovement Dracula;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Dracula = FindObjectOfType<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public float Get_Dmg()
+    {
+        return damage;
+    }
+
+    public float Get_Def()
+    {
+        return defense;
+    }
+
+    public float Get_Exp()
+    {
+        return expYield;
+    }
+
+    public void TakeDamage(float Dmg)
+    {
+        health -= Dmg;
+        if(health <= 0)
+        {
+            Death();
+        }
+        StartCoroutine(invincibility());
+    }
+
+    private void Death()
+    {
+        animator.SetBool("Dead", true);
+        vulerable = false;
+        Dracula.AddXP(expYield);
+        Physics2D.IgnoreCollision(collider2, Dracula.collider2);
+    }
+
+    IEnumerator invincibility()
+    {
+        vulerable = false;
+        yield return new WaitForSeconds(1);
+        if(health > 0)
+        {
+            vulerable = true;
+        }
+    }
+}
--- Project Conquest/Assets/Scripts/PlayerMovement.cs
+++ Project Conquest/Assets/Scripts/PlayerMovement.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
 using UnityEditor;
 using UnityEditor.UI;
 using UnityEngine;
@@ -13,17 +14,24 @@ public class PlayerMovement : MonoBehaviour
     public float health;
     public float damage;
     public float defense;
-    public float Speed;
-    public float Jump;
+    public float speed;
+    public float jump;
+    public float attackMultiplier;
 
     [Header("Calculation Variables")]
-    public float SpeedCap;
-    public float Acceleration;
+    public float speedCap;
+    public float acceleration;
+    public float attackRange;
 
     [Header("Components")]
-    public Rigidbody2D Body;
-    public Camera MainCamera;
+    public Rigidbody2D body;
+    public Camera mainCamera;
     public Animator animator;
+    public Transform attackPoint;
+    public Collider2D collider2;
+
+    [Header("Extras")]
+    public LayerMask enemies;
 
     Vector2 velocity = new Vector2(0, 0);
 
@@ -31,112 +39,137 @@ public class PlayerMovement : MonoBehaviour
     bool boost;
     bool moving;
     bool attack;
-
-    float SpeedCache;
-    float SpeedCapCache;
-    float JumpCache;
-    float ScaleCache;
-
     //functionally a bool that alternates between -1 and 1.
     int right = 1;
 
+    float speedCache;
+    float speedCapCache;
+    float jumpCache;
+    float scaleCache;
+    float damageCache;
+
+    int experience = 0;
+
+    string STATE;
+
     void Start()
-    { 
-        SpeedCache = Speed;
-        SpeedCapCache = SpeedCap;
-        JumpCache = Jump;
-        ScaleCache = transform.localScale.x;
+    {
+        STATE = "Default";
+        speedCache = speed;
+        speedCapCache = speedCap;
+        jumpCache = jump;
+        scaleCache = transform.localScale.x;
+        damageCache = damage;
     }
 
     void Update()
     {
         isGrounded = Physics2D.IsTouchingLayers(GetComponent<Collider2D>(), LayerMask.GetMask("Ground"));
-        MainCamera.transform.position = new Vector3(transform.position.x, transform.position.y,transform.position.z -5);
-        Run();
-        Jumping();
-        Attack();
-        Crouch();
-        Direction();
+        mainCamera.transform.position = new Vector3(transform.position.x, transform.position.y,transform.position.z -5);
+        switch (STATE)
+        {
+            case "Default":
+                Run();
+                Jumping();
+                Attack();
+                Crouch();
+                Direction();
+                break;
+            case "Drinking":
+                Run();
+                Jumping();
+                Direction();
+                Crouch();
+                break;
+        }
     }
 
+    //Control Functions
     private void Direction()
     {
-        if (Input.GetAxis("Horizontal") < 0)
+        if (attack == false)
         {
-            transform.localScale = new Vector3(ScaleCache * -1, transform.localScale.y, transform.localScale.z);
-            right = -1;
-        }
-        if (Input.GetAxis("Horizontal") > 0)
-        {
-            transform.localScale = new Vector3(ScaleCache, transform.localScale.y, transform.localScale.z);
-            right = 1;
+            if (Input.GetAxis("Horizontal") < 0)
+            {
+                transform.localScale = new Vector3(scaleCache * -1, transform.localScale.y, transform.localScale.z);
+                right = -1;
+            }
+            if (Input.GetAxis("Horizontal") > 0)
+            {
+                transform.localScale = new Vector3(scaleCache, transform.localScale.y, transform.localScale.z);
+                right = 1;
+            }
         }
     }
-
+    
     private void Jumping()
     {
-        if (Input.GetAxis("Jump") != 0 && isGrounded)
+        if (Input.GetAxis("Jump") != 0 && isGrounded && animator.GetBool("Crouch") == false)
         {
-            velocity.x = Body.velocity.x;
-            velocity.y = Jump * Input.GetAxis("Jump");
-            Body.velocity = velocity;
+            velocity.x = body.velocity.x;
+            velocity.y = jump * Input.GetAxis("Jump");
+            body.velocity = velocity;
         }
-        if (isGrounded == false && Input.GetAxis("Vertical") < -0.75 && Body.velocity.y < 3)
+        if (isGrounded == false && Input.GetAxis("Vertical") < -0.75 && body.velocity.y < 3)
         {
-            velocity.x = Body.velocity.x * .3f;
-            velocity.y = -Jump * 2;
-            Body.velocity = velocity;
+            velocity.x = body.velocity.x * .3f;
+            velocity.y = -jump * 2;
+            body.velocity = velocity;
         }
     }
 
     private void Run()
     {
-        if (Input.GetAxis("Horizontal") != 0 && attack == false && animator.GetBool("Crouch") == false)
+        if (Input.GetAxis("Horizontal") != 0 && animator.GetBool("Crouch") == false)
         {
             StartCoroutine(AmIStopped());
             StartCoroutine(RunTimer());
-            if (Speed < SpeedCap)
+            if (speed < speedCap)
             {
-                Speed += (SpeedCache / SpeedCapCache) * Acceleration;
+                speed += (speedCache / speedCapCache) * acceleration;
             }
-            velocity.x = Speed * Input.GetAxis("Horizontal");
-            velocity.y = Body.velocity.y;
-            Body.velocity = velocity;
+            velocity.x = speed * Input.GetAxis("Horizontal");
+            velocity.y = body.velocity.y;
+            body.velocity = velocity;
         }
 
         if (Input.GetAxis("Horizontal") == 0 && animator.GetBool("Crouch") == false)
         {
             moving = false;
             boost = false;
-            Jump = JumpCache;
-            SpeedCap = SpeedCapCache;
-            Speed = (Speed + SpeedCache) / 2;
-            velocity.x = Speed * Input.GetAxis("Horizontal");
-            velocity.y = Body.velocity.y;
-            Body.velocity = velocity;
+            jump = jumpCache;
+            speedCap = speedCapCache;
+            speed = (speed + speedCache) / 2;
+            velocity.x = speed * Input.GetAxis("Horizontal");
+            velocity.y = body.velocity.y;
+            body.velocity = velocity;
         }
     }
 
     private void Attack()
     {
-
+        //Slide
         if (Input.GetAxis("Fire1") != 0 && attack == false && animator.GetBool("Crouch") == true)
         {
             attack = true;
             animator.SetTrigger("Attack");
-            velocity.x = SpeedCapCache * right;
-            velocity.y = Body.velocity.y;
-            Body.velocity = velocity;
+            damage = damage * attackMultiplier;
+            velocity.x = speedCapCache * right;
+            velocity.y = body.velocity.y;
+            body.velocity = velocity;
         }
 
+        //Attack
         if (Input.GetAxis("Fire1") != 0 && attack == false)
         {
+            speed = speed / 2;
             attack = true;
             animator.SetTrigger("Attack");
+            damage = damage * attackMultiplier;
         }
 
     }
-       
+
     private void Crouch()
     {
         if (Input.GetAxis("Vertical") < 0 && isGrounded == true && animator.GetBool("Crouch") == false)
@@ -149,18 +182,54 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+    //Animation Functions
+    private void DumbassAttack()
+    {
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemies);
+
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            HumanController human = enemy.GetComponent<HumanController>();
+            if(human.vulerable == true)
+            {
+                human.TakeDamage(DamageCalculator(human.Get_Def()));
+
+                Vector3 knockback = enemy.transform.position - transform.position + Vector3.up * 0.33f;
+                enemy.gameObject.GetComponent<Rigidbody2D>().velocity = knockback * 10;
+            }
+        }
+    }
+
     private void AttackDone()
     {
         attack = false;
+        damage = damageCache;
     }
 
     private void SlideDone()
     {
         velocity.x = 0;
-        velocity.y = Body.velocity.y;
-        Body.velocity = velocity;
+        velocity.y = body.velocity.y;
+        body.velocity = velocity;
     }
 
+    private void ReturnDefault()
+    {
+        STATE = "Default";
+    }
+
+    //Calculation Functions
+    private float DamageCalculator(float Def)
+    {
+        if(attack == true)
+        {
+            //add damage multiplier
+        }
+        float DMG = damage/Def + 1;
+        return DMG;
+    }
+
+    //Enumerators
     IEnumerator RunTimer()
     {
         if (moving == true)
@@ -169,8 +238,8 @@ public class PlayerMovement : MonoBehaviour
 
             if (boost == false && moving == true)
             {
-                SpeedCap = SpeedCap * 1.25f;
-                Jump = Jump * 1.5f;
+                speedCap = speedCap * 1.25f;
+                jump = jump * 1.5f;
                 boost = true;
             }
             if (boost == true)
@@ -180,8 +249,8 @@ public class PlayerMovement : MonoBehaviour
         }
         else
         {
-            Jump = JumpCache;
-            SpeedCap = SpeedCapCache;
+            jump = jumpCache;
+            speedCap = speedCapCache;
             yield break;
         }
 
@@ -190,13 +259,68 @@ public class PlayerMovement : MonoBehaviour
     {
         float Gposition = GetComponent<Transform>().position.x;
         yield return new WaitForSeconds(2);
-        if(Gposition == GetComponent<Transform>().position.x)
+        if (Gposition == GetComponent<Transform>().position.x)
         {
             moving = false;
-        } else
+        }
+        else
         {
             moving = true;
         }
 
+    }
+
+    //Engine Functions
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 10 && collision.gameObject.GetComponent<HumanController>().vulerable == true)
+        {
+            //Enemy Damage is greater than player defense
+            if (collision.gameObject.GetComponent<HumanController>().Get_Dmg() > defense)
+            {
+                Vector3 knockback = collision.transform.position - transform.position + Vector3.up * 0.33f;
+                body.velocity = knockback * 10;
+            }
+            //Enemy Damage is less than player defense
+            else
+            {
+
+            }
+            //Player Damage is greater than Enemy Defense
+            if (damage > collision.gameObject.GetComponent<HumanController>().Get_Def())
+            {
+                Vector3 knockback = collision.transform.position - transform.position + Vector3.up * 0.33f;
+                collision.gameObject.GetComponent<Rigidbody2D>().velocity = knockback * 10;
+            }
+            //Player Damage is less than Enemy Defense
+            else
+            {
+
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 10)
+        {
+            STATE = "Drinking";
+            print("gotcha");
+            if(Input.GetAxis("Fire1") != 0 && collision.gameObject.GetComponent<HumanController>().vulerable == false)
+            {
+                print("huh");
+                animator.SetTrigger("Drink");
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+
+    public void AddXP(int gains)
+    {
+        experience += gains;
     }
 }

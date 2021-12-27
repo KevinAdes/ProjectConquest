e387b29d19a5385440db5c4440a643a931f2ab4b
KevinAdes <kra18@hampshire.edu>
2021-12-27 15:44:46 -0500

    Level Vol 2
    adjusted field of view script to work with turrets, made it easier to add future adjustments. created turrets to add variety to levels

--- Project Conquest/Assets/Scripts/FieldOfView.cs
+++ Project Conquest/Assets/Scripts/FieldOfView.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http.Headers;
 using UnityEngine;
 
@@ -10,11 +11,23 @@ public class FieldOfView : MonoBehaviour
     [Range(0,360)]
     public float viewAngle;
 
+    public float offset;
+
     public LayerMask TargetMask;
     public LayerMask Obstacles;
 
     public int direction;
-    //eventually this may have to be changed to allow for non humans to have a field of view.
+
+    string TYPE;
+
+    //This Script will be attached to many different game objects and will
+    //impact them differently. I don't know if this process can be
+    //automated so for now I will add object types to this list and
+    //manually select what kind of object has a field of view via the
+    //start method. Eventually I would like to make it work so that I don't
+    //have to update this script everytime i make a new thing-that-needs-a-field-of-view.
+
+    Turret turret;
     HumanController human;
 
     public void Start()
@@ -22,13 +35,22 @@ public class FieldOfView : MonoBehaviour
         if(GetComponent<HumanController>() != null)
         {
             human = GetComponent<HumanController>();
-
+            TYPE = "HUMAN";
+        }
+        if(GetComponent<Turret>() != null)
+        {
+            turret = GetComponent<Turret>();
+            TYPE = "TURRET";
         }
     }
 
     public void Update()
     {
-        direction = GetComponent<HumanController>().right;
+        if (human != null)
+        {
+            direction = GetComponent<HumanController>().right;
+
+        }
         FindVisibleTargets();
     }
 
@@ -51,15 +73,51 @@ public class FieldOfView : MonoBehaviour
             Transform targetPos = target.transform;
             Vector3 dirToTarget = (targetPos.position - transform.position).normalized;
 
-            
             if (Vector3.Angle(transform.right * direction,dirToTarget) < viewAngle/2)
             {
                 float distace = Vector3.Distance(transform.position, targetPos.position);
-                if (!Physics2D.Raycast(transform.position, dirToTarget, distace, Obstacles) && !human.spooked){
-                    human.spooked = true;
-                    GetComponent<Animator>().SetTrigger("Spooked");
+                if (!Physics2D.Raycast(transform.position, dirToTarget, distace, Obstacles)){
+                    switch (TYPE)
+                    {
+                        case "HUMAN":
+                            HumanInit();
+                            break;
+                        case "TURRET":
+                            TurretInit();
+                            break;
+                    }
                 }
             }
+            //handles removing targets if they leave the radius
+            if((Vector3.Distance(target.transform.position, transform.position)-.5f) > viewRadius)
+            {
+                switch (TYPE)
+                {
+                    case "HUMAN":
+                        break;
+                    case "TURRET":
+                        turret.detected = false;
+                        break;
+                }
+            }
+        }
+    }
+
+    private void HumanInit()
+    {
+        if (human != null && human.spooked == false)
+        {
+            human.spooked = true;
+            GetComponent<Animator>().SetTrigger("Spooked");
+        }
+    }
+
+    private void TurretInit()
+    {
+        if (turret != null && turret.detected == false)
+        {
+            turret.range = viewRadius;
+            turret.detected = true;
         }
     }
 }
---
+++ Project Conquest/Assets/Scripts/Turret.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    public GameObject projectile;
+    public float fireRate;
+    public float force;
+    float shotSpacing = 0;
+
+    public float range;
+    public Transform target;
+    public bool detected = false;
+    Vector2 direction = new Vector2(0, 0);
+    PlayerMovement Dracula;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Dracula = FindObjectOfType<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (detected)
+        {
+            case true:
+                Seek();
+                break;
+            case false:
+                break;
+        }
+    }
+
+    private void Seek()
+    {
+        target = Dracula.transform;
+        Vector2 targetPosition = target.position;
+        direction = targetPosition - (Vector2)transform.position;
+        RaycastHit2D rayinfo = Physics2D.Raycast(transform.position, direction, range);
+        transform.up = -direction;
+
+        if (Time.time > shotSpacing)
+        {
+            shotSpacing = Time.time + 1 / fireRate;
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        GameObject BulletInst = Instantiate(projectile, transform.position, Quaternion.identity);
+        BulletInst.GetComponent<Rigidbody2D>().AddForce(direction * force);
+    }
+}
